09f58c3f542905d9b1867abc94ea1f0c53f2d4b7
Codier-Grotte <107997873+Codier-Grotte@users.noreply.github.com>
2022-07-04 22:31:54 +0200

    Update script.js
    added comments

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};        /* If you scroll down, below function will be executed*/
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {    /* If you scroll down more than 80 pixels, the settings below will be executed,
+                                                                                        if below 80 pixels scrolling down is not reached, just stays the same */
     document.getElementById("navbar").style.fontSize = "20px";
     document.getElementById("logo").style.fontSize = "20px";
   } else {
